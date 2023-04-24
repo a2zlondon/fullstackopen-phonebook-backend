defc00162284a823fdf7dace488689305b714a0b
Daniel Molloy <d4nmolloy@gmail.com>
2023-04-24 11:56:25 +0100

    Exercise 3.9 Changed to use local server

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 
+const cors = require('cors')
 const app = express()
 
+
+app.use(cors())
 app.use(express.json())
 
 morgan.token('req-body', function (req, res) {
@@ -98,7 +101,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
