fafa62523ba15733baf6cb13dcac6a1bc8f18d94
Daniel Molloy <d4nmolloy@gmail.com>
2023-05-07 15:10:07 +0100

    Exercise 3.15

--- index.js
+++ index.js
@@ -55,10 +55,12 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        person.deleteOne().then(persons => { response.send(`deleted ${person.name}`); response.status(204).end() })
-    })
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
