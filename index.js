c0557d2e6a9468c0e51f58c96dbad09ee6d33736
Daniel Molloy <d4nmolloy@gmail.com>
2023-05-07 15:42:26 +0100

    Exercise 3.16 error fandling middleware

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 require('dotenv').config();
+
+
 const Person = require('./models/person')
-app.use(express.static('build'))
-app.use(cors())
-app.use(express.json())
 
 morgan.token('req-body', function (req, res) {
     const body = JSON.stringify(req.body)
@@ -14,6 +13,26 @@ morgan.token('req-body', function (req, res) {
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
 
 app.get('/info', (request, response) => {
     let serverDate = new Date();
@@ -28,10 +47,16 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -39,9 +64,7 @@ app.post('/api/persons', (request, response) => {
     const body = request.body
 
     if ((!body.name)||(!body.number)) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+        return response.status(400).json({ error: 'content missing' })
     }
 
     const person = new Person({
@@ -78,7 +101,10 @@ app.put('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
